bb5000cb5b10ea8797828d283e68d112ac2ae1f8
Hasan BINBOGA <hasanbinboga@gmail.com>
2024-03-05 14:47:18 +0300

    initial commit

---
+++ src/May.Siber/May.Siber.TestCase.Tests/UnitTest1.cs
@@ -0,0 +1,120 @@
+namespace May.Siber.TestCase.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		private Solution solution;
+        public UnitTest1()
+        {
+            solution = new Solution();
+        }
+
+        [TestMethod("0 Kutu - Liste boþ")]
+		public void NoItemTest()
+		{
+			var K = new int[0];
+			var expected = 0;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod("1 Kutu - Listede sadece bir kutu var")]
+		public void OneBoxTest()
+		{
+			var K = new int[] { 4 };
+			var expected = 4;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+
+		[TestMethod("2 Kutu - Büyük kutu son eleman")]
+		public void TwoBoxTest1()
+		{
+			var K = new int[] { 2, 4 };
+			//Böyle bir durumda tek koli hesaplamalý ve iki kutuyu tek koliye koymalý
+			//Bu durumda kolinin boyutu 4x2 = 8 olmalý.
+			var expected = 4*2;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod("2 Kutu - Ýki kutu da ayný")]
+		public void TwoBoxTest2()
+		{
+			var K = new int[] { 2, 2 };
+			//Böyle bir durumda tek koli hesaplamalý ve iki kutuyu tek koliye koymalý
+			//Bu durumda kolinin boyutu 2x2 = 4 olmalý.
+			var expected = 2 * 2;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod("3 Kutu - Dizi: 2,1,3")]
+		public void ThreeBoxTest1()
+		{
+			var K = new int[] { 2, 1, 3 };
+			//Böyle bir durumda tek koli hesaplamalý ve iki kutuyu tek koliye koymalý
+			//Bu durumda kolinin boyutu 3x3 = 9 olmalý.
+			var expected = 3 * 3;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod("3 Kutu - Dizi: 2,3,1")]
+		public void ThreeBoxTest2()
+		{
+			var K = new int[] { 2, 3, 1 };
+			//Böyle bir durumda iki koli hesaplamalý ve son kutuyu ikinci koliye koymalý
+			//Bu durumda kolilerin toplam boyutu 3x2 + 1x1 = 6 +1 = 7  olmalý.
+			var expected = 3 * 2 + 1*1;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod("3 Kutu - Dizi: 4,3,1")]
+		public void ThreeBoxTest3()
+		{
+			var K = new int[] { 4, 3, 1 };
+			//Böyle bir durumda iki koli hesaplamalý ve son iki kutuyu ikinci koliye koymalý
+			//Bu durumda kolilerin toplam boyutu 4x1 + 3x2 = 4 + 6 = 10  olmalý.
+			var expected = 4 * 1 + 3 * 2;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod("3 Kutu - Dizi: 4,4,1")]
+		public void ThreeBoxTest4()
+		{
+			var K = new int[] { 4, 4, 1 };
+			//Böyle bir durumda iki koli hesaplamalý ve son kutuyu ikinci koliye koymalý
+			//Bu durumda kolilerin toplam boyutu 4x2 + 1x1 = 8 + 1 = 9  olmalý.
+			var expected = 4 * 2 + 1 * 1;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+
+		[TestMethod("3 Kutu - Dizi: 4,4,4")]
+		public void ThreeBoxTest5()
+		{
+			var K = new int[] { 4, 4, 4 };
+			//Böyle bir durumda tek koli hesaplamalý
+			//Bu durumda kolilerin toplam boyutu 4x3 = 12  olmalý.
+			var expected = 4 * 3;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod("3 Kutu - Dizi: 4,1,4")]
+		public void ThreeBoxTest6()
+		{
+			var K = new int[] { 4, 1, 4 };
+			//Böyle bir durumda tek koli hesaplamalý
+			//Bu durumda kolilerin toplam boyutu 4x3 = 12  olmalý.
+			var expected = 4 * 3;
+			var actual = solution.solution(K);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ src/May.Siber/May.Siber.TestCase/Solution.cs
@@ -0,0 +1,67 @@
+﻿namespace May.Siber.TestCase
+{
+	public class Solution
+	{
+
+		public int solution(int[] K)
+		{
+			//Eğer kutu yoksa 
+			if (K.Length == 0) return 0;
+
+			//Eğer 1 kutu varsa
+			if (K.Length == 1) return K[0];
+
+			//Eğer 2 kutu varsa
+			if(K.Length == 2)
+			{
+				if (K[0] > K[1]) return K[0]*2;
+
+				return K[1]*2;
+			}
+
+
+			//İlk olarak en uzun kutunun konumunu bul
+			var firstLongestBoxId = GetLongestBox(K, out int longestBoxHeight);
+
+			//İlk Kolinin alanı, en uzun koliyi içerecek şekilde hesaplanır.
+			var firstPackageSize = longestBoxHeight * (firstLongestBoxId + 1);
+
+			//En uzun koliden sonraki kutuları ikinci koliye yerleştirebiliriz. 
+			//Bunun için kalan kutuları yeni bir diziye alıp en uzun olanı hesaplamamız yeterli olacaktır.
+			var secondPackageWidth = K.Length - firstLongestBoxId - 1;
+			var secondPackageItems = new List<int>(secondPackageWidth);
+			if (K.Length - 1 > firstLongestBoxId)
+			{
+				for (var i = firstLongestBoxId+1; i < K.Length; i++)
+				{
+					secondPackageItems.Add(K[i]);
+				}
+
+			}
+
+
+			var L = secondPackageItems.ToArray();
+			GetLongestBox(L, out longestBoxHeight);
+
+			var secondPackageSize = longestBoxHeight * secondPackageWidth;
+
+			return firstPackageSize + secondPackageSize;
+		}
+
+		private int GetLongestBox(int[] K, out int maxSize)
+		{
+			var maxIdx = 0;
+			maxSize = int.MinValue;
+			for (int i = 0; i < K.Length; i++)
+			{
+				if (K[i] >= maxSize)
+				{
+					maxIdx = i;
+					maxSize = K[i];
+				}
+			}
+			return maxIdx;
+		}
+
+	}
+}
